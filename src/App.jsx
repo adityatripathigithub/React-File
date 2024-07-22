a2696e3560684744a66a33e3eade0e87999ea363
adity tripathi <tripathiaditya163@gmail.com>
2024-07-22 23:59:24 +0530

    use usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
+// import React, { useState } from 'react'
+
+// const App = () => {
+
+//   const [aditya, setaditya] = useState("Bhai")
+//   const handchange = () => {
+  
+//     useState
+//   }
+
+
+
+//   return (
+//     <div>
+//       <h1 onClick={handchange}>mere dosto</h1>
+//       <h1>{aditya}</h1>
+
+//     </div>
+//   )
+// }
+
+// export default App ;
+
+
 import React, { useState } from 'react'
 
 const App = () => {
 
-  const [aditya, setaditya] = useState("Bhai")
-  const handchange = () => {
-  }
+  let a = 30;
+   
+  const [number, setnumber] = useState(30)
 
 
   return (
-    <div>
-      <h1 onClick={handchange}>mere dosto</h1>
-      <h1>{aditya}</h1>
-
-    </div>
+    <div onClick={hedingchang}>App</div>
   )
 }
 
-export default App ;
+export default App
