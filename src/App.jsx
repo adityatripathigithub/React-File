b2fd7e5333dc9468ae3dd7e6861e8ff9e88d4c68
adity tripathi <tripathiaditya163@gmail.com>
2024-07-24 23:59:18 +0530

    Create a Form and applying 2way Binding

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,55 @@ import React, { useState } from 'react'
 const App = () => {
 
 
-  const [name, setname] = useState("Hello Mera Name")
-  const hendchange = () => {
+  // const [name, setname] = useState("Hello Mera Name")
+  // const hendchange = () => {
 
-    setname(name + " ADITYA TRIPATHI Hai")
+  //   setname(name + " ADITYA TRIPATHI Hai")
+
+  // }
+
+
+  const [name, setName] = useState('')
+  const [age, setAge] = useState('')
+  const [email, setEmail] = useState('')
+  const [data, setdata] = useState({})
+
+  let clutter = ''
+  const handelclick = (e) => {
+
+    e.preventDefault()
+    setdata({ name, age, email })
 
   }
 
+
   return (
-    <div>
+    //     <div>
 
-      <button onClick={hendchange} >Create Your Full Name</button>
+    //       <button onClick={hendchange} >Create Your Full Name</button>
 
-      <h1>{name}</h1>
+    //       <h1>{name}</h1>
 
 
-    </div>
+    //     </div>
+    //   )
+
+    // }
+
+    <>
+      <form action="">
+        <input type="text" value={name} onChange={e => setName(e.target.value)} />
+        <input type="text" value={age} onChange={e => setAge(e.target.value)} />
+        <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+        <button onClick={e => handelclick(e)}>submit</button>
+      </form>
+
+      <h1>{data.name}</h1>
+      <h1>{data.age}</h1>
+      <h1>{data.email}</h1>
+    </>
+
   )
-
 }
 
 
